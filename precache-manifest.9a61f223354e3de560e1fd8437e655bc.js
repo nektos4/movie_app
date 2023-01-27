0e156c2a3859dc95e8f5df7e8cd0ba9d2c20df78
Nikita Belkin <nikitachanell26@gmail.com>
2023-01-27 20:47:42 +0300

    Updates

---
+++ precache-manifest.9a61f223354e3de560e1fd8437e655bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a20a0c92ec4b15cb007",
+    "url": "/movie_app/static/css/main.f15635fd.chunk.css"
+  },
+  {
+    "revision": "9a20a0c92ec4b15cb007",
+    "url": "/movie_app/static/js/main.9a20a0c9.chunk.js"
+  },
+  {
+    "revision": "5c9ad1c654e346e4d4a0",
+    "url": "/movie_app/static/js/1.5c9ad1c6.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "4fdedee451c4ff570bce78c4c0beb2eb",
+    "url": "/movie_app/index.html"
+  }
+];
